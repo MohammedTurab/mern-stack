5f6098f51e4e804aafa13a02e51f805e5a7be788
MohammedTurab <mohammedturab7799@gmail.com>
2023-02-15 22:10:14 +0530

    mongodb

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose=require("mongoose");
+
+
 const app = express();
 const port = process.env.PORT || 5010;
 
@@ -6,7 +10,18 @@ app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html")
 	
 });
-
+mongoose.set('strictQuery',true);
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING,{},function(err){
+	if(err){
+		console.error(err);
+	}
+	else{
+		console.log("DB connected");
+		app.listen(port,function(){
+			console.log("server started at port no 3000");
+		});
+	}
+});
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
 	console.log(`Server running on http://localhost:${port}`);
